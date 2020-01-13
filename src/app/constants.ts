8c2cd4aab78edfd6dad7d7da76874fa7c1f28195
meikai <meikai@seyes.org>
2020-01-13 13:05:53 +0100

    publish to dev

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:4200';
-export const API_URL = 'http://localhost:8080/api';
+export const BASE_URL = 'https://dev.bgov.de';
+export const API_URL = 'https://api.bgov.de/api';
 export const API_KEY = 'AIzaSyDQWbU1SdJuusI9t2sp1AqpzIIqX3y1W9c';
 export const AUTH_DOMAIN = 'bgov-web.firebaseapp.com';
